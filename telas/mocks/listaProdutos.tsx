c1747a9f62a6842349de6a326ed8b33a16e30995
Lucas <167233250+LudWig192@users.noreply.github.com>
2026-05-15 21:44:24 -0300

    Telas Produtos

---
+++ telas/mocks/listaProdutos.tsx
@@ -0,0 +1,20 @@
+const lista_produto = {
+    itens: {
+        titulo: "Veja nossos produtos!",
+        lista: [
+            {
+                id: 1,
+                nome: "Jogos de banheiro",
+                descricao: "Contém capa para tampa, tapete e porta papel",
+                imagem: require('../../assets/Risque astral.webp'),
+            },
+            {
+                id: 2,
+                nome: "Jogo Americano",
+                descricao: "4pcs. Temos outros modelos e cores.",
+                imagem: require('../../assets/icon.png'),
+            }
+        ]
+    }
+}
+export default lista_produto;
